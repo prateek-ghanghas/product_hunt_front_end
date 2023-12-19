4c5b63ee637d2587901303d80d3d4a74d39a7f1a
prateek-ghanghas <prateekghnghs2000@gmail.com>
2023-12-19 23:44:39 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,19 +5,47 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import './App.css';
 import Button from '@mui/material/Button';
 import Box from '@mui/system/Box';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Comment_api from './Comment_api';
 import Unfollow from './Unfollow';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
  
   const [alignment, setAlignment] = useState('UNFOLLOW')
+  
+  
+  const {loginWithRedirect,user,isAuthenticated,loginWithPopup,logout} = useAuth0()
 
+  
+  useEffect(() => {
+  if(isAuthenticated){ 
+    const object = {
+      method: "post",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+               },
+     body : JSON.stringify({
+      gmail : user.email,
+      name: user.name
+     }) 
+} 
+    console.log(user.email)
+  fetch('https://6ljg9361sl.execute-api.eu-north-1.amazonaws.com/v1/userData',object)}
 
+  },[isAuthenticated]
+
+  )
+
+  console.log(isAuthenticated)
  
   const toogle_button = (e,newAlignment) => {
     setAlignment(newAlignment)
   }
+
+  console.log(user)
+
 
   
   
@@ -25,11 +53,11 @@ function App() {
    <>
    
 
-  
+   {isAuthenticated ?<h1> welcome {user.name}</h1>: <h1>welcome</h1>}
    <Box width = '100%' sx={{display : 'flex', flexDirection : 'row',justifyContent : 'center', alignItems : 'center'}}>
    <Grid pl={5} container bgcolor = '#bebeb6' sx={{height : '44px',alignItems : 'center',justifyContent: 'center',width : '1000px',borderRadius : '10px'}} ml={32} mt={2} mr={32}>
     <Grid item color= '#5E5353' mr={90}><Typography color={'black'}>PH mate</Typography></Grid>
-    <Grid item><Button sx={{backgroundColor : '#333333',height : '28px', color : 'white', borderRadius : '10px','&:hover' :{backgroundColor : '#8E969D'}}}>Sign Up</Button></Grid>
+    <Grid item>{isAuthenticated? <Button onClick={() => logout()}>Log out</Button>:<Button onClick={() => loginWithPopup()} sx={{backgroundColor : '#333333',height : '28px', color : 'white', borderRadius : '10px','&:hover' :{backgroundColor : '#8E969D'}}}>Sign Up</Button>}</Grid>
    </Grid>
    </Box>
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <Auth0Provider
+  domain="dev-weqiq7vh7u4akork.us.auth0.com"
+    clientId="ogKoz86T4c6DFjM2O74QAYx3qZmr9PNR"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
     <App />
+    </Auth0Provider>
   
 );
 
