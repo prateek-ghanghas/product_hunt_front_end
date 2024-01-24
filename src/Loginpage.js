f3f9562c7823fd816ab02e2a80351c4d97aa9ea2
prateek-ghanghas <prateekghnghs2000@gmail.com>
2024-01-25 02:02:23 +0530

    saved

--- src/Loginpage.js
+++ src/Loginpage.js
@@ -1,7 +1,10 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import { Button ,Box, Typography} from '@mui/material'
+import { Button ,Box, Typography,Stack} from '@mui/material'
 import React, { useEffect } from 'react'
 import { Navigate, useNavigate } from 'react-router-dom'
+import './landingPage.css'
+import EastIcon from '@mui/icons-material/East';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 
 function Loginpage() {
     const {loginWithPopup,isAuthenticated} = useAuth0()
@@ -18,10 +21,45 @@ function Loginpage() {
 
   return (
     <>
-    <Box sx={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column',alignContent:'center',height:'100vh',justifyContent:'space-evenly'}}>
-        <Typography>Please Log in to use</Typography>
-        <Button onClick={handleclick} variant='contained'>Log in</Button>
+    <div className='wrapper'>
+      <Box sx={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',pt:'150px',width:'100%'}}>
+    <Stack sx={{color:'white',fontSize:'50px'}}>
+      Let's Grow Together On Product Hunt!
+    </Stack>
     </Box>
+    <Stack sx={{pl:'430px',textAlign:'center', color:'wheat',fontSize:'20px',display:'flex',flexDirection:'row', justifyContent:'center',alignItems:'center',width:'700px',alignSelf:'center',justifyItems:'center',justifySelf:'center',textTransform:'capitalize'}}>
+      <Typography sx={{fontSize:'h6.fontSize',fontFamily:'helvectica'}}>
+      <p>
+
+      Optimize your product hunt journey with our simple web app! 
+Increase your followers and engagement by Automate commenting on top newly launched products,by removing non-followback users. Maximize your network and maintain a relevant follower base  
+      </p>
+      </Typography>
+    </Stack>
+    <Box>
+
+    </Box>
+      
+
+    
+    
+    <Box sx={{display:'flex',justifyContent:'center',alignItems:'center',alignContent:'center',mt:'20px'}}>
+      <Box sx={{color:'#E9DBC6'}}><Typography sx={{fontFamily:'helvectica',fontSize:'30px'}}><b>Start using the app straight away &nbsp;</b></Typography></Box>
+        <Button sx={{padding: '7px 14px',
+    fontSize: '1.2rem',
+    color: '#fff',
+    fontWeight: '600',
+    textTransform: 'uppercase',
+    border: '1px solid rgb(249, 168, 168)',
+    borderRadius: '4px',
+    cursor: 'pointer' ,':hover':{background:'white',
+      color: 'black'
+
+      }}} endIcon={<EastIcon/>} onClick={handleclick} className='login-button'>Log in</Button>
+    </Box>
+    </div>
+  
+  
     </>
   )
 }
